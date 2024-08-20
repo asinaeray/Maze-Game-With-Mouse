c3c476896dbb6fb3c2cd82a16a791b73263c4d3e
Eray Aşina <162134586+asinaeray@users.noreply.github.com>
2024-08-20 20:29:24 +0300

    Maze Game With Mouse
    Reach the end of the maze with the mouse

---
+++ labirentoyunu/labirentoyunu/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labirentoyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("duvara dokundunuz!!");
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("tebrikler,kazandınız!!");
+        }
+    }
+}
